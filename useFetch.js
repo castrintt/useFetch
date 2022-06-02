41f8b8380143ac23f95a6fbefc417f49139041f1
Igor castro <96383031+castrintt@users.noreply.github.com>
2022-06-02 11:31:09 -0300

    Update useFetch.js

--- useFetch.js
+++ useFetch.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useContext } from "react"
-import AuthContext from "../context/AuthContext"
 
 const useFetch = (url, met) => {
     let { authTokens } = useContext(AuthContext)
